0c1932c854aacbe7d4272026df973ae70f487a88
Angelina Julia de França Mendonça de Melo <angelina.melo@dbserver.com.br>
2024-07-30 18:36:32 -0300

    Register Tests

--- src/test/java/pages/register/RegisterPage.java
+++ src/test/java/pages/register/RegisterPage.java
@@ -1,6 +1,7 @@
 package pages.register;
 
 import java.time.Duration;
+import java.util.function.BooleanSupplier;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,6 +24,11 @@ public class RegisterPage extends PageObject{
         driver.findElement(By.xpath("//*[@id='__next']/div/div[2]/div/div[1]/form/div[3]/button[2]")).click();
     }
 
+    public boolean isRegisterDisplayed(){
+        return driver.findElement(By.xpath("//form[@class='styles__ContainerFormRegister-sc-7fhc7g-0 keVbpY']")).isDisplayed();
+    }
+
+
     public void fillRegisterFilds(String email, String name, String password, String passwordConfirmation) {
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@class='styles__ContainerFormRegister-sc-7fhc7g-0 keVbpY']")));
@@ -42,13 +48,15 @@ public class RegisterPage extends PageObject{
     }
 
     public boolean successMessage() {
+        return driver.getPageSource().contains("foi criada com sucesso");
         
-        return driver.findElement(By.xpath("//div[@class='styles__ContainerContent-sc-8zteav-1 cSdWPv']")).isDisplayed();
+        //return driver.findElement(By.xpath("//div[@class='styles__ContainerContent-sc-8zteav-1 cSdWPv']"));
         
     }
 
     public String getUserAcountFromScreen () {
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("modalText")));
         String countCriatedMessage = driver.findElement(By.id("modalText")).getText();
         String countNumber = countCriatedMessage.split(" ")[2];
         System.out.println(countNumber);
@@ -60,7 +68,37 @@ public class RegisterPage extends PageObject{
         driver.findElement(By.xpath("//a[@id='btnCloseModal']")).click();
         return new LoginPage(driver);
     }
+
+	public void addBalance() {
+		driver.findElement(By.xpath("//label[@class='styles__Container-sc-1pngcbh-0 kIwoPV']")).click();
+	}
+
+    public boolean backToLoginForm() {
+        return driver.findElement(By.xpath("//form[@class='style__ContainerFormLogin-sc-1wbjw6k-0 eTrcYr']")).isDisplayed();
+    }
     
+    public boolean msgErrorRequiredFild() {
+        return driver.getPageSource().contains("É campo obrigatório");
+
+    }
+
+    public boolean isDisplayedMsgErrorDifferentPasswords() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("modalText")));
+        return driver.getPageSource().contains("As senhas não são iguais.");
+
+    }
+
+    public boolean isDisplayedMsgErrorInvalidFormat() {
+        return driver.getPageSource().contains("Formato inválido");
+
+    }
+
+    public boolean isDisplayedMsgErrorNameEmpty() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("modalText")));
+
+        return driver.getPageSource().contains("Nome não pode ser vazio.");
+
+    }
 
     
 }
--- src/test/java/pages/register/RegisterTest.java
+++ src/test/java/pages/register/RegisterTest.java
@@ -16,7 +16,7 @@ public class RegisterTest {
 	}
 
     @Test
-    public void shouldRegisterSuccesfully(){
+    public void shouldRegisterSuccesfullyWithoutCash(){
         registerPage.goToRegisterPage();
 
         registerPage.fillRegisterFilds("a@email.com", "Fulana", "senha123", "senha123");
@@ -30,5 +30,108 @@ public class RegisterTest {
         Assertions.assertEquals(registerPage.getUserAcountFromScreen(), registerPage.getAccountNumber());
         
         registerPage.closeModal(); 
+
+        Assertions.assertTrue(registerPage.backToLoginForm());
+    }
+
+    @Test
+    public void shouldRegisterSuccesfullyWithCash(){
+        registerPage.goToRegisterPage();
+
+        registerPage.fillRegisterFilds("a@email.com", "Fulana", "senha123", "senha123");
+
+        registerPage.addBalance();
+        
+        registerPage.submitRegiterForm();
+
+        registerPage.getUserAcountFromScreen();
+
+        Assertions.assertTrue(registerPage.successMessage());
+
+        Assertions.assertEquals(registerPage.getUserAcountFromScreen(), registerPage.getAccountNumber());
+        
+        registerPage.closeModal(); 
+        Assertions.assertTrue(registerPage.backToLoginForm());
+    }
+
+    @Test
+    public void shouldNotRegisterWithoutEmail(){
+        registerPage.goToRegisterPage();
+
+        registerPage.fillRegisterFilds("", "Fulana", "12345678", "12345678");
+        
+        registerPage.submitRegiterForm();
+
+        Assertions.assertTrue(registerPage.msgErrorRequiredFild());
+        Assertions.assertTrue(registerPage.isRegisterDisplayed());
+        
+    }
+
+    @Test
+    public void shouldNotRegisterWithEmailInvalid(){
+        registerPage.goToRegisterPage();
+
+        registerPage.fillRegisterFilds("a@email", "Fulana", "12345678", "12345678");
+        
+        registerPage.submitRegiterForm();
+
+        Assertions.assertTrue(registerPage.isDisplayedMsgErrorInvalidFormat());
+        Assertions.assertTrue(registerPage.isRegisterDisplayed());
+
+        
+    }
+
+    @Test
+    public void shouldNotRegisterWithoutPassword(){
+        registerPage.goToRegisterPage();
+
+        registerPage.fillRegisterFilds("a@email.com", "Fulana", "", "123456");
+        
+        registerPage.submitRegiterForm();
+
+        Assertions.assertTrue(registerPage.msgErrorRequiredFild());
+        Assertions.assertTrue(registerPage.isRegisterDisplayed());
+        
+    }
+    @Test
+    public void shouldNotRegisterWithoutConfirmationPassword(){
+        registerPage.goToRegisterPage();
+
+        registerPage.fillRegisterFilds("a@email.com", "Fulana", "123456", "");
+        
+        registerPage.submitRegiterForm();
+
+        Assertions.assertTrue(registerPage.msgErrorRequiredFild());
+        Assertions.assertTrue(registerPage.isRegisterDisplayed());
+
+    }
+
+    @Test
+    public void shouldNotRegisterWithoutNameFilled(){
+        registerPage.goToRegisterPage();
+
+        registerPage.fillRegisterFilds("a@email.com", "", "123456", "123456");
+        
+        registerPage.submitRegiterForm();
+
+        Assertions.assertTrue(registerPage.isDisplayedMsgErrorNameEmpty());
+        Assertions.assertTrue(registerPage.isRegisterDisplayed());
+        Assertions.assertFalse(registerPage.successMessage());
+
+    }
+
+    @Test
+    public void shouldNotRegisterWithoutDiferentPasswords(){
+        registerPage.goToRegisterPage();
+
+        registerPage.fillRegisterFilds("a@email.com", "Fulana", "1234", "5678");
+        
+        registerPage.submitRegiterForm();
+
+        Assertions.assertTrue(registerPage.isDisplayedMsgErrorDifferentPasswords());
+        Assertions.assertTrue(registerPage.isRegisterDisplayed());
+        
+
+
     }
 }
